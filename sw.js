0fe2a1206eef22ed827f8e16e5f4f16fe1efc3d9
aatopini <aatopini@gmail.com>
2025-10-19 19:43:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,30 @@
-const CACHE = 'power-hour-v1';
+const CACHE = 'ph-cache-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+  // add './style.css', './app.js' here if you split files out later
 ];
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('activate', evt => {
-  evt.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener('fetch', evt => {
-  const req = evt.request;
-  if(req.method !== 'GET') return;
-  evt.respondWith(
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
     caches.match(req).then(cached => cached || fetch(req))
   );
 });
